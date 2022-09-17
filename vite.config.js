044e62604d3a382f8c787f70aa6c745ee3ebff23
davidhckh <david.heckhoff@gmail.com>
2022-09-17 18:59:26 +0200

    :tada: First commit

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+import path from "path";
+
+const dirname = path.resolve();
+
+export default defineConfig({
+  root: "sources",
+  publicDir: "../public",
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+    sourcemap: true,
+  },
+});
